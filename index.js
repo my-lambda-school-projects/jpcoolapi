0c1c958486a165672d854dbb03a76fd08c351b58
jamespagedev <jamespagedev@outlook.com>
2019-01-10 17:07:11 -0700

    makes port dynamic

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ const server = require('./api/server.js');
 
 // console.log(server.secret);
 
-server.listen(5000, () => console.log('server on port 5k'));
+// enable dynamic ports for heroku (or other hosting provider)
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`server on port: ${port}`));
